b52b6ff3cb864498d7fedf3243956beee7e3bc13
Ganesh Ambagol <ambagol@Ganeshs-MacBook-Pro.local>
2018-11-27 20:49:07 -0800

    Deploy to Azure

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+
+const compliments= [
+    "you look nice today",
+    "that dress looks good",
+    "thats hot",
+    "you are amazing",
+    "you have a nice shirt",
+    "this is awesome",
+    "nice beard"
+]
+
+function getRandomCompliment(){
+    const randomIndex= Math.floor(Math.random() * compliments.length)
+    return compliments[randomIndex];
+}
+
+const app = express();
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname,"index.html"));
+})
+
+app.get("/compliment", function(req,res){
+    res
+    .json({
+        compliment:getRandomCompliment()
+    })
+    .end();
+})
+
+app.use("/public", express.static("./public"))
+
+app.listen(3000);
+
+console.log("listening on http://localhost:3000");
+
